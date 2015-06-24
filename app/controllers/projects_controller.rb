4464ebe1cb3f23a9b20ce0e1394202f901a6b2e2
joshuabamboo <joshuaowens011@gmail.com>
2015-06-24 11:25:10 +0200

    allow ability to edit projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_filter :authorize
-  
+
   def new
     @project = Project.new
   end
@@ -20,6 +20,20 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      # Handle a successful update.
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
 
   resources :projects
   resources :users
