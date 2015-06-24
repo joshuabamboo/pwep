c168244978b0527856a99d1b3be8e53334839b93
joshuabamboo <joshuaowens011@gmail.com>
2015-06-24 09:49:33 +0200

    project flow

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render "new"
+    end
+  end
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+
+private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :total_days)
+  end
 end
+
