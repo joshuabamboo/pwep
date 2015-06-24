b16ed99721aef7716d9cc62f6d9200f7d701de2d
joshuabamboo <joshuaowens011@gmail.com>
2015-06-24 16:55:25 +0200

    add feature model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :features
+   accepts_nested_attributes_for :features
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624075740) do
+ActiveRecord::Schema.define(version: 20150624124005) do
+
+  create_table "features", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string  "title"
