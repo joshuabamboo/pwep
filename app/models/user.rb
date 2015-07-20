d83ca75521ba4f91ad26e2c2cc75cc94ecc352a8
joshuabamboo <joshuaowens011@gmail.com>
2015-07-20 18:23:53 -0400

    add username and image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,20 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_auth_hash(auth)
     binding.pry
-    user = User.find_or_create_by(id: auth["id"])
-    # self.where(id: auth["id"]).first_or_initialize.tap do |user|
-    #   user.access_token = auth.credentials.token
-    #   user.github_username = auth.info.nickname
-    #   user.provider = auth.provider
-    #   user.uid = auth.uid
-    #   user.email = auth.info.email
-    #   user.image = auth.info.image
+    # user = User.find_or_create_by(id: auth["id"])
+    self.where(id: auth["id"]).first_or_initialize.tap do |user|
+      user.name = auth["name"]
+      user.username = auth["login"]
+      user.email = auth["email"]
+      user.image = auth["avatar_url"]
     #   user.admin = user.is_admin?
-    #   user.save
-    # end
+      user.save
+    end
+  end
+
+  private
+
+  def is_admin?
+    #find out if they are a member of flatiron curriculum
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717173303) do
+ActiveRecord::Schema.define(version: 20150720222300) do
 
   create_table "features", force: :cascade do |t|
     t.string   "title"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20150717173303) do
     t.string  "email"
     t.boolean "admin"
     t.string  "password_digest"
+    t.string  "image"
+    t.string  "username"
   end
 
 end
